0d09a3f3fb6e33263da557f1de527c843d0ec9d7
DevExpressExampleBot <fakeEmail>
2026-01-30 12:27:53 +0400

    Source auto update [skip ci]

--- VB/WinForms_Localizer/WinForms_Localizer/MyDashboardWinLocalizer.vb
+++ VB/WinForms_Localizer/WinForms_Localizer/MyDashboardWinLocalizer.vb
@@ -1,13 +1,13 @@
-﻿Imports DevExpress.DashboardWin.Localization
-Imports DevExpress.Utils.Localization
+Imports DevExpress.DashboardWin.Localization
 Imports System
 
 Namespace WinForms_Localizer
+
     Friend Class MyDashboardWinLocalizer
         Inherits DashboardWinLocalizer
 
         Public Overrides Function GetLocalizedString(ByVal id As DashboardWinStringId) As String
-            Return System.Enum.GetName(GetType(DashboardWinStringId), id)
+            Return [Enum].GetName(GetType(DashboardWinStringId), id)
         End Function
     End Class
 End Namespace
